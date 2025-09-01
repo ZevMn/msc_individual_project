fa995b3608bdee5bc6a06297ffb33dcdaa8e6c58
Zev Menachemson <zm1224@imperial.ac.uk>
2025-09-01 15:06:44 +0100

    Updated and cleaned up experiments.py

--- experiments/embeddings/experiments.py
+++ experiments/embeddings/experiments.py
@@ -6,33 +6,55 @@ Example usage:
     run_*_experiment(encoder_to_evaluate="imagenet", feat_mode="all", dataset="Mammo")
 """
 
+"""
+experiments/embeddings/experiment.py
+
+Defines the four core experiment workflows for analysing embeddings across datasets:
+    1. Visualisation of embeddings and shifts.
+    2. Shift quantification using KL divergence.
+    3. Initial detection rate analysis for different shifts.
+    4. Bootstrap detection rate analysis for different shifts.
+
+Example usage:
+--------------
+    run_*_experiment(
+        encoder_to_evaluate="imagenet",
+        feat_mode="all",
+        dataset="Mammo",
+        force_calculations=False
+    )
+"""
+
 from experiments.embeddings.config import Config
 from experiments.embeddings import plotting_utils as plotting
 from experiments.embeddings import data_processing_utils as data_processing
 from experiments.embeddings import statistical_utils as statistical
 
 
-# -----------------------------------------
-# First experiment called in main execution
-# -----------------------------------------
+# ---------------------------
+# 1. Visualisation experiment
+# ---------------------------
 def run_visualisation_experiment(
     encoder_to_evaluate: str,
     feat_mode: str,
     dataset: str,
     force_calculation: bool = False,
 ) -> None:
+    """
+    Runs dimensionality reduction (PCA, t-SNE) and plots embeddings representations,
+    as well as comparisons of simulated shifts.
+
+    Args:
+        encoder_to_evaluate (str): Encoder key (see Config.ENCODERS).
+        feat_mode (str): Feature mode (see Config.FEAT_MODES_MAP).
+        dataset (str): Dataset name (see Config.DATASET_CONFIG).
+        force_calculation (bool): Recalculate projections even if cached results exist.
+    """
 
     Config.validate()
     Config.set_seeds()
 
-    output_dir = (
-        Config.ROOT
-        / "experiments"
-        / "outputs"
-        / dataset
-        / "Visualisations"
-        / encoder_to_evaluate
-    )
+    output_dir = Config.ROOT / "experiments" / "outputs" / "Visualisations" / dataset
     output_dir.mkdir(parents=True, exist_ok=True)
 
     print(
@@ -68,6 +90,7 @@ def run_visualisation_experiment(
 
     layer_to_results_dict = data_processing.calculate_and_save_layer_pca_and_tsne(
         output_dir=output_dir,
+        encoder_to_evaluate=encoder_to_evaluate,
         layers=layers,
         val_embeddings=val_embeddings,
         test_embeddings=test_embeddings,
@@ -76,14 +99,6 @@ def run_visualisation_experiment(
     )
 
     # Experiment 1: Visualisation of embeddings
-    plotting.plot_layer_representations_scatter(
-        output_dir=output_dir,
-        dataset=dataset,
-        encoder_to_evaluate=encoder_to_evaluate,
-        layer_to_results_dict=layer_to_results_dict,
-        labels=val_labels,
-        shift="no_shift",
-    )
     plotting.plot_layer_representations_jointplot(
         output_dir=output_dir,
         dataset=dataset,
@@ -100,49 +115,102 @@ def run_visualisation_experiment(
         encoder_to_evaluate=encoder_to_evaluate,
         layer_to_results_dict=layer_to_results_dict,
     )
-    # plotting.plot_shift_comparison_scatter(
-    #     output_dir=output_dir,
-    #     dataset=dataset,
-    #     encoder_to_evaluate=encoder_to_evaluate,
-    #     layer_to_results_dict=layer_to_results_dict,
-    # )
 
-    print(f"\n=== VISUALIZATION COMPLETE ===\n")
+    print(f"\n=== VISUALISATION COMPLETE ===\n")
 
 
-# ------------------------------------------
-# Second experiment called in main execution
-# ------------------------------------------
+# ----------------------------------
+# 2. Shift quantification experiment
+# ----------------------------------
+def run_shift_quantification_experiment(
+    encoder_to_evaluate: str,
+    feat_mode: str,
+    dataset: str,
+    force_calculations: bool = False,
+) -> None:
+    """
+    Quantifies covariate shifts using KL divergence with bootstrapping.
+
+    Args:
+        encoder_to_evaluate (str): Encoder key (see Config.ENCODERS).
+        feat_mode (str): Feature mode (see Config.FEAT_MODES_MAP).
+        dataset (str): Dataset name (see Config.DATASET_CONFIG).
+        force_calculations (bool): Recalculate statistics even if cached results exist.
+    """
+
+    Config.validate()
+    Config.set_seeds()
+
+    output_dir = Config.ROOT / "experiments" / "outputs" / "ShiftQuantification"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    print(
+        f"\n=== {dataset.upper()} | {encoder_to_evaluate.upper()} | {feat_mode.upper()} ===\n"
+    )
+
+    # Process test and val CSVs
+    val_df, test_df = data_processing.load_csvs_and_add_idx_column(dataset=dataset)
+
+    # Generate or load embeddings
+    encoder_output = data_processing.generate_and_load_embeddings(
+        encoder_to_evaluate=encoder_to_evaluate,
+        feat_mode=feat_mode,
+        dataset=dataset,
+        val_df=val_df,
+        test_df=test_df,
+    )
+
+    # Validate the embeddings output and extract layers, and val/test splits
+    _, val_embeddings, test_embeddings = (
+        data_processing.validate_and_process_embeddings(encoder_output=encoder_output)
+    )
+
+    # Generate covariate-shifted test subsets and store their original indices
+    shift_to_indices_dict = data_processing.simulate_wide_range_of_shifts(
+        dataset=dataset, test_df=test_df
+    )
+
+    # Calculate KL divergences for all simulated shifts with bootstrap
+    statistical.calculate_kl_div_for_all_shifts_all_layers(
+        output_dir=output_dir,
+        dataset=dataset,
+        encoder_to_evaluate=encoder_to_evaluate,
+        val_embeddings=val_embeddings,
+        test_embeddings=test_embeddings,
+        shift_to_indices_dict=shift_to_indices_dict,
+        force_calculations=force_calculations,
+        n_bootstrap=100,
+    )
+
+    # Generate plots
+    plotting.plot_kl_panels_multi_layer(input_dir=output_dir, output_dir=output_dir)
+
+    print(f"\n=== SHIFT QUANTIFICATION COMPLETE ===\n")
+
+
+# ------------------------------------
+# 3. Initial detection rate experiment
+# ------------------------------------
 def run_detection_rate_experiment(
     encoder_to_evaluate: str,
     feat_mode: str,
     dataset: str,
     force_calculations: bool = False,
 ) -> None:
-    # We have a given dataset and encoder.
-    # We want to loop through the different shift scenarios.
-    # Ignore mixed shifts. Look at e.g. "acquisition" and "population" shifts.
-    # For each type of shift, we want to analyse "subtle", "moderate" and "extreme".
-    # -> Produce a table with MMD outputs from each layer of the encoder.
-    # -> (Make sure to subsample properly - look at Mel's function).
-    # -> For each row, output the correct data as a csv/json.
-    # -> Create a separate script to run and collect the data and process it into a table.
-    # -> Plot this table as a heat map, colour coded by detection rate.
-    # Ultimately, we will produce one table for each dataset and encoder.
-    # The hypothesis is that different shift types will have different detection rates at different layers.
-    # Consider adding other metrics to compare.
+    """
+    Estimates detection rates of different shift types and severities.
+
+    Args:
+        encoder_to_evaluate (str): Encoder key (see Config.ENCODERS).
+        feat_mode (str): Feature mode (see Config.FEAT_MODES_MAP).
+        dataset (str): Dataset name (see Config.DATASET_CONFIG).
+        force_calculations (bool): Recalculate detection rates even if cached results exist.
+    """
 
     Config.validate()
     Config.set_seeds()
 
-    output_dir = (
-        Config.ROOT
-        / "experiments"
-        / "outputs"
-        / dataset
-        / "DetectionRates"
-        / encoder_to_evaluate
-    )
+    output_dir = Config.ROOT / "experiments" / "outputs" / "DetectionRates" / dataset
     output_dir.mkdir(parents=True, exist_ok=True)
 
     print(
@@ -194,12 +262,12 @@ def run_detection_rate_experiment(
         output_dir=output_dir, dataset=dataset, encoder_to_evaluate=encoder_to_evaluate
     )
 
-    print(f"\n=== STAT CALCULATIONS COMPLETE ===\n")
+    print(f"\n=== DETECTION RATE ANALYSIS COMPLETE ===\n")
 
 
-# -----------------------------------------
-# Third experiment called in main execution
-# -----------------------------------------
+# --------------------------------------
+# 4. Bootstrap detection rate experiment
+# --------------------------------------
 def run_bootstrap_experiment(
     encoder_to_evaluate: str,
     feat_mode: str,
@@ -207,7 +275,14 @@ def run_bootstrap_experiment(
     force_calculations: bool = False,
 ) -> None:
     """
-    Runs the bootstrap experiment to calculate detection rates for covariate shifts.
+    Runs bootstrap experiments to robustly quantify detection rates for
+    a range of sample sizes and shift severities.
+
+    Args:
+        encoder_to_evaluate (str): Encoder key (see Config.ENCODERS).
+        feat_mode (str): Feature mode (see Config.FEAT_MODES_MAP).
+        dataset (str): Dataset name (see Config.DATASET_CONFIG).
+        force_calculations (bool): Recalculate detection rates even if cached results exist.
     """
     Config.validate()
     Config.set_seeds()
@@ -261,69 +336,7 @@ def run_bootstrap_experiment(
         force_calculations=force_calculations,
     )
 
-    # Experiment 4: Bootstrap detection rates analysis
     # Plot the bootstrap results
     plotting.plot_all_bootstrap_results(output_dir)
 
-    print(f"\n=== BOOTSTRAP CALCULATIONS COMPLETE ===\n")
-
-
-# ------------------------------------------
-# Fourth experiment called in main execution
-# ------------------------------------------
-def run_shift_quantification_experiment(
-    encoder_to_evaluate: str,
-    feat_mode: str,
-    dataset: str,
-    force_calculations: bool = False,
-) -> None:
-
-    Config.validate()
-    Config.set_seeds()
-
-    output_dir = Config.ROOT / "experiments" / "outputs" / "ShiftQuantification"
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    print(
-        f"\n=== {dataset.upper()} | {encoder_to_evaluate.upper()} | {feat_mode.upper()} ===\n"
-    )
-
-    # Process test and val CSVs
-    val_df, test_df = data_processing.load_csvs_and_add_idx_column(dataset=dataset)
-
-    # Generate or load embeddings
-    encoder_output = data_processing.generate_and_load_embeddings(
-        encoder_to_evaluate=encoder_to_evaluate,
-        feat_mode=feat_mode,
-        dataset=dataset,
-        val_df=val_df,
-        test_df=test_df,
-    )
-
-    # Validate the embeddings output and extract layers, and val/test splits
-    _, val_embeddings, test_embeddings = (
-        data_processing.validate_and_process_embeddings(encoder_output=encoder_output)
-    )
-
-    # Generate covariate-shifted test subsets and store their original indices
-    shift_to_indices_dict = data_processing.simulate_wide_range_of_shifts(
-        dataset=dataset, test_df=test_df
-    )
-
-    # Calculate and plot KL divergences for all simulated shifts
-    kl_divs = statistical.calculate_kl_div_for_all_shifts(
-        output_dir=output_dir,
-        dataset=dataset,
-        encoder_to_evaluate=encoder_to_evaluate,
-        val_embeddings=val_embeddings,
-        test_embeddings=test_embeddings,
-        shift_to_indices_dict=shift_to_indices_dict,
-        layer="after_maxpool",
-        force_calculations=force_calculations,
-    )
-    plotting.plot_kl_scatter(
-        output_dir=output_dir,
-        dataset=dataset,
-        encoder_to_evaluate=encoder_to_evaluate,
-        kl_divs=kl_divs,
-    )
+    print(f"\n=== BOOTSTRAP ANALYSIS COMPLETE ===\n")
